b599ddba252be67344c146e3379b7d9724a88734
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-03-19 13:32:54 +0000

    Mar 19, 2024, 6:32 AM

--- helpers.cpp
+++ helpers.cpp
@@ -1,19 +1,20 @@
-#include "helpers.h"
-Region binaryFind(const std::vector<Region>& regions, int id)
-{
-  int left = 0;
-  int right = regions.size() - 1;
+// #include "helpers.h"
+// Region binaryFind(const std::vector<Region>& regions, int id)
+// {
+//   int left = 0;
+//   int right = regions.size() - 1;
 
-  while (left <= right) {
-    int mid = left + (right - left) / 2;
-    if (regions[mid].id == id) {
-      return regions[mid];
-    } else if (regions[mid].id < id) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
-  // Return a default Region if not found
-  return Region();
-}
+//   while (left <= right) {
+//     int mid = left + (right - left) / 2;
+//     if (regions[mid].id == id) {
+//       return regions[mid];
+//     } else if (regions[mid].id < id) {
+//       left = mid + 1;
+//     } else {
+//       right = mid - 1;
+//     }
+//   }
+//   // Return a default Region if not found
+//   return Region();
+// }
+// why linking .cpp and .h been not working
