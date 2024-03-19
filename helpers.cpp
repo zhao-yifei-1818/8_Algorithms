0db8a462bdf22dc97f576882b1b4ebc869ac3f20
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-03-19 06:06:29 +0000

    Mar 18, 2024, 11:06 PM

--- helpers.cpp
+++ helpers.cpp
@@ -1,22 +1,4 @@
 #include "helpers.h"
-#include <fstream>
-#include <iostream>
-#include <sstream>
-
-void readRegions(std::vector<Region>& regions)
-{
-  std::ifstream file("ZILLOW_REGIONS.csv");
-  std::string line;
-  while (std::getline(file, line)) {
-    std::stringstream ss(line);
-    std::string id, city, state;
-    std::getline(ss, id, ',');
-    std::getline(ss, city, ',');
-    std::getline(ss, state, ',');
-    regions.push_back({std::stoi(id), city, state});
-  }
-  file.close();
-}
 
 void printFirstFiveRegions(const std::vector<Region>& regions)
 {
--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include "helpers.h"
-#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
+#include <vector>
+void readRegions(std::vector<Region>& regions)
+{
+  std::ifstream file("ZILLOW_REGIONS.csv");
+  std::string line;
+  while (std::getline(file, line)) {
+    std::stringstream ss(line);
+    std::string id, city, state;
+    std::getline(ss, id, ',');
+    std::getline(ss, city, ',');
+    std::getline(ss, state, ',');
+    regions.push_back({std::stoi(id), city, state});
+  }
+  file.close();
+}
 int main()
 {
   std::vector<Region> regions;
