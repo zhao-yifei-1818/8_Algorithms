5d25b544284a3b6e4e8a2425e195136c509effe3
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-03-19 05:26:54 +0000

    Mar 18, 2024, 10:26 PM

--- helpers.h
+++ helpers.h
@@ -1,1 +1,21 @@
+#ifndef HELPERS_H
+#define HELPERS_H
 
+#include <string>
+#include <vector>
+
+struct Region {
+  int id;
+  std::string city;
+  std::string state;
+
+  bool operator<(const Region& other) const
+  {
+    return id < other.id;
+  }
+};
+
+void readRegions(std::vector<Region>& regions);
+// void print(const std::vector<Region>& regions);
+
+#endif // HELPERS_H
