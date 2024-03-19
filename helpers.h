a71d22e3c77364e8e61bf0c064a614b65ae5e8b7
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-03-19 06:17:35 +0000

    Mar 18, 2024, 11:17 PM

--- helpers.h
+++ helpers.h
@@ -7,6 +7,23 @@ struct Region {
   int id;
   std::string city;
   std::string state;
-};
 
+  bool operator<(const Region& other) const
+  {
+    return id < other.id;
+  }
+};
+template<typename T>
+void insertionSort(std::vector<T>& vec)
+{
+  for (int i = 1; i < vec.size(); i++) {
+    T key = vec[i];
+    int j = i;
+    while (j > 0 && key < vec[j - 1]) {
+      swap(vec[j], vec[j - 1]);
+      j--;
+    }
+    vec[j] = key;
+  }
+};
 #endif // HELPERS_H
