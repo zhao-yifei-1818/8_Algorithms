8958891d382ecf548ef9022248c7d8c89d2285ff
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-03-19 06:23:54 +0000

    Mar 18, 2024, 11:23 PM

--- helpers.h
+++ helpers.h
@@ -18,12 +18,11 @@ struct Region {
 template<typename T>
 void insertionSort(std::vector<T>& vec)
 {
-  for (size_t i = 1; i < vec.size(); ++i) {
+  for (int i = 1; i < vec.size(); i++) {
     T key = vec[i];
-    size_t j = i;
+    int j = i;
     while (j > 0 && key < vec[j - 1]) {
-      vec[j] = vec[j - 1];
-      --j;
+      swap(vec[j], vec[j - 1];) j--;
     }
     vec[j] = key;
   }
